c3f79d045430f81dceaa243e89d37a5066a31fb8
wgj839977837 <839977837@qq.com>
2021-11-12 19:09:18 +0800

    add stop data augment parameter

--- tools/combin_multi_model_results.py
+++ tools/combin_multi_model_results.py
@@ -10,11 +10,12 @@ from pcdet.utils import calibration_kitti, object3d_kitti, box_utils
 from pcdet.ops.iou3d_nms.iou3d_nms_utils import nms_weighted_gpu
 
 
-path_pp = "/home/zhangxiao/code/OpenPCDet/output/kitti_models/pointpillar/default/eval/epoch_7728/val/default/final_result/data"
-path_se = "/home/zhangxiao/code/OpenPCDet/output/kitti_models/second/default/eval/epoch_7862/val/default/final_result/data"
-save_path = "/home/zhangxiao/tmp"
-calib_root_path = "/data1/segmentation_detection_fuse_development/data/kitti/training/calib"
-
+path_pp = "/home/wangguojun/source_code/OpenPCDet/output/cfgs/kitti_models/cyv_det_metav6_fuse2_raw_transformer_mask/final_car_ped_cyc"
+path_se = "/home/wangguojun/source_code/OpenPCDet/output/cfgs/kitti_models/cyv_det_metav6_fuse2_raw_transformer_mask_test_cyclist_ped/label_merge"
+save_path = "/home/wangguojun/source_code/OpenPCDet/output/cfgs/kitti_models/cyv_det_metav6_fuse2_raw_transformer_mask_test_cyclist_ped/multi_model_label"
+calib_root_path = "/data/kitti/testing/calib"
+from pathlib import Path
+Path(save_path).mkdir(exist_ok=True,parents=True)
 def get_label_from_path(label_file):
     sample_idx = label_file.split('/')[-1].split('.')[0]
     calib_file_path = os.path.join(calib_root_path, sample_idx + ".txt")
@@ -23,35 +24,39 @@ def get_label_from_path(label_file):
 
     assert os.path.exists(calib_file_path)
     calib = calibration_kitti.Calibration(calib_file_path)
+    # print("leng obj is ",len(obj_list))
+    if len(obj_list)>0:
+        annotations = {}
+        annotations['name'] = np.array([obj.cls_type for obj in obj_list])
+        annotations['truncated'] = np.array([obj.truncation for obj in obj_list])
+        annotations['occluded'] = np.array([obj.occlusion for obj in obj_list])
+        annotations['alpha'] = np.array([obj.alpha for obj in obj_list])
+        annotations['bbox'] = np.concatenate([obj.box2d.reshape(1, 4) for obj in obj_list], axis=0)
+        annotations['dimensions'] = np.array([[obj.l, obj.h, obj.w] for obj in obj_list])  # lhw(camera) format
+        annotations['location'] = np.concatenate([obj.loc.reshape(1, 3) for obj in obj_list], axis=0)
+        annotations['rotation_y'] = np.array([obj.ry for obj in obj_list])
+        annotations['score'] = np.array([obj.score for obj in obj_list])
+        annotations['difficulty'] = np.array([obj.level for obj in obj_list], np.int32)
 
-    annotations = {}
-    annotations['name'] = np.array([obj.cls_type for obj in obj_list])
-    annotations['truncated'] = np.array([obj.truncation for obj in obj_list])
-    annotations['occluded'] = np.array([obj.occlusion for obj in obj_list])
-    annotations['alpha'] = np.array([obj.alpha for obj in obj_list])
-    annotations['bbox'] = np.concatenate([obj.box2d.reshape(1, 4) for obj in obj_list], axis=0)
-    annotations['dimensions'] = np.array([[obj.l, obj.h, obj.w] for obj in obj_list])  # lhw(camera) format
-    annotations['location'] = np.concatenate([obj.loc.reshape(1, 3) for obj in obj_list], axis=0)
-    annotations['rotation_y'] = np.array([obj.ry for obj in obj_list])
-    annotations['score'] = np.array([obj.score for obj in obj_list])
-    annotations['difficulty'] = np.array([obj.level for obj in obj_list], np.int32)
+        num_objects = len([obj.cls_type for obj in obj_list if obj.cls_type != 'DontCare'])
+        num_gt = len(annotations['name'])
+        index = list(range(num_objects)) + [-1] * (num_gt - num_objects)
+        annotations['index'] = np.array(index, dtype=np.int32)
 
-    num_objects = len([obj.cls_type for obj in obj_list if obj.cls_type != 'DontCare'])
-    num_gt = len(annotations['name'])
-    index = list(range(num_objects)) + [-1] * (num_gt - num_objects)
-    annotations['index'] = np.array(index, dtype=np.int32)
+        loc = annotations['location'][:num_objects]
+        dims = annotations['dimensions'][:num_objects]
+        rots = annotations['rotation_y'][:num_objects]
+        loc_lidar = calib.rect_to_lidar(loc)
+        l, h, w = dims[:, 0:1], dims[:, 1:2], dims[:, 2:3]
+        loc_lidar[:, 2] += h[:, 0] / 2
 
-    loc = annotations['location'][:num_objects]
-    dims = annotations['dimensions'][:num_objects]
-    rots = annotations['rotation_y'][:num_objects]
-    loc_lidar = calib.rect_to_lidar(loc)
-    l, h, w = dims[:, 0:1], dims[:, 1:2], dims[:, 2:3]
-    loc_lidar[:, 2] += h[:, 0] / 2
+        score = annotations['score'][:num_objects]
+        name = annotations['name'][:num_objects]
+        gt_boxes_lidar = np.concatenate([loc_lidar, l, w, h, -(np.pi / 2 + rots[..., np.newaxis]),
+                                         score[..., np.newaxis], name[..., np.newaxis]], axis=1)
+    else:
+        gt_boxes_lidar=np.zeros((0,9))
 
-    score = annotations['score'][:num_objects]
-    name = annotations['name'][:num_objects]
-    gt_boxes_lidar = np.concatenate([loc_lidar, l, w, h, -(np.pi / 2 + rots[..., np.newaxis]),
-                                     score[..., np.newaxis], name[..., np.newaxis]], axis=1)
     return gt_boxes_lidar, calib
 
 def get_template_prediction(num_samples):
@@ -113,25 +118,34 @@ for ann_file_name in all_files:
     bboxes = [box[:-1] for box in gt_bbox]
     bboxes = torch.from_numpy(np.array(bboxes, np.float64)).cuda().float()
     
-    box_dict = {}
-    for idx, cls_ in enumerate(cls_list):
-        cls_index = (names == cls_)
-        bbox = bboxes[cls_index]
-        score = bbox[:, -1]
-        if bbox.size(0) != 0:
-            bbox, keep = nms_weighted_gpu(bbox[:, :7].contiguous(), score, thresh=0.1)
-            if "pred_scores" not in box_dict.keys():
-                box_dict["pred_scores"] = score[keep]
-                box_dict["pred_boxes"] = bbox[keep]
-                box_dict["pred_labels"] = torch.zeros_like(score[keep]).int() + idx
 
-            else:
-                box_dict["pred_scores"] = torch.cat((box_dict["pred_scores"], score[keep]))
-                box_dict["pred_boxes"] = torch.cat((box_dict["pred_boxes"], bbox[keep]), dim=0)
-                tmp_score = torch.zeros_like(score[keep]).int() + idx
-                box_dict["pred_labels"] = torch.cat((box_dict["pred_labels"], tmp_score))
+    if bboxes.shape[0]>0:
+        box_dict = {}
+        for idx, cls_ in enumerate(cls_list):
+            cls_index = (names == cls_)
+            bbox = bboxes[cls_index]
+            score = bbox[:, -1]
+            if bbox.size(0) != 0:
+                bbox, keep = nms_weighted_gpu(bbox[:, :7].contiguous(), score, thresh=0.1)
+                if "pred_scores" not in box_dict.keys():
+                    box_dict["pred_scores"] = score[keep]
+                    box_dict["pred_boxes"] = bbox[keep]
+                    box_dict["pred_labels"] = torch.zeros_like(score[keep]).int() + idx
+
+                else:
+                    box_dict["pred_scores"] = torch.cat((box_dict["pred_scores"], score[keep]))
+                    box_dict["pred_boxes"] = torch.cat((box_dict["pred_boxes"], bbox[keep]), dim=0)
+                    tmp_score = torch.zeros_like(score[keep]).int() + idx
+                    box_dict["pred_labels"] = torch.cat((box_dict["pred_labels"], tmp_score))
+    else:
+        box_dict = {
+            'pred_scores': torch.zeros(0).cuda(),
+            'pred_boxes': torch.zeros([0, 7]).cuda(),
+            'pred_labels': torch.zeros(0).cuda(),
+        }
 
     single_pred_dict = generate_single_sample_dict(box_dict, calib)
+
     cur_det_file = os.path.join(save_path, ann_file_name)
     with open(cur_det_file, 'w') as f:
         bbox = single_pred_dict['bbox']
--- tools/train.py
+++ tools/train.py
@@ -174,7 +174,8 @@ def main():
         lr_warmup_scheduler=lr_warmup_scheduler,
         ckpt_save_interval=args.ckpt_save_interval,
         max_ckpt_save_num=args.max_ckpt_save_num,
-        merge_all_iters_to_one_epoch=args.merge_all_iters_to_one_epoch
+        merge_all_iters_to_one_epoch=args.merge_all_iters_to_one_epoch,
+        stop_aug_epoch=cfg.STOP_AUG_EPOCHS
     )
 
     logger.info('**********************End training %s/%s(%s)**********************\n\n\n'
