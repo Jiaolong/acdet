d27ceef59c334c88d3494fc81b4c68b6b3b6ff17
zhangxiao <563562675@qq.com>
2021-11-16 19:54:58 +0800

    Clean ops

--- pcdet/ops/iou3d_nms/iou3d_nms_utils.py
+++ pcdet/ops/iou3d_nms/iou3d_nms_utils.py
@@ -7,8 +7,7 @@ import torch
 
 from ...utils import common_utils
 from . import iou3d_nms_cuda
-import numba
-import numpy as np
+
 
 def boxes_bev_iou_cpu(boxes_a, boxes_b):
     """
@@ -101,14 +100,8 @@ def nms_gpu(boxes, scores, thresh, pre_maxsize=None, **kwargs):
 
 
 def nms_weighted_gpu(boxes, scores, thresh, pre_maxsize=None, post_max_size=None, **kwargs):
-    # order = scores.sort(0, descending=True)[1]
-    # if pre_maxsize is not None:
-    #     order = order[:pre_maxsize]
-    # boxes = boxes[order].contiguous()
-
     keep = torch.zeros(boxes.size(0), dtype=torch.long)
     num_out = iou3d_nms_cuda.nms_gpu(boxes, keep, thresh)
-    # keep = order[keep[:num_out].cuda(boxes.device)].contiguous()
     keep = keep[:num_out].cuda(boxes.device).contiguous()
     if post_max_size is not None:
         keep = keep[:post_max_size]
@@ -135,46 +128,3 @@ def nms_normal_gpu(boxes, scores, thresh, **kwargs):
     keep = torch.LongTensor(boxes.size(0))
     num_out = iou3d_nms_cuda.nms_normal_gpu(boxes, keep, thresh)
     return order[keep[:num_out].cuda()].contiguous(), None
-
-
-@numba.jit(nopython=True)
-def circle_nms(dets, thresh, post_max_size=83):
-    """Circular NMS.
-
-    An object is only counted as positive if no other center
-    with a higher confidence exists within a radius r using a
-    bird-eye view distance metric.
-
-    Args:
-        dets (torch.Tensor): Detection results with the shape of [N, 3].
-        thresh (float): Value of threshold.
-        post_max_size (int): Max number of prediction to be kept. Defaults
-            to 83
-
-    Returns:
-        torch.Tensor: Indexes of the detections to be kept.
-    """
-    x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    scores = dets[:, 2]
-    order = scores.argsort()[::-1].astype(np.int32)  # highest->lowest
-    ndets = dets.shape[0]
-    suppressed = np.zeros((ndets), dtype=np.int32)
-    keep = []
-    for _i in range(ndets):
-        i = order[_i]  # start with highest score box
-        if suppressed[
-                i] == 1:  # if any box have enough iou with this, remove it
-            continue
-        keep.append(i)
-        for _j in range(_i + 1, ndets):
-            j = order[_j]
-            if suppressed[j] == 1:
-                continue
-            # calculate center distance between i and j box
-            dist = (x1[i] - x1[j])**2 + (y1[i] - y1[j])**2
-
-            # ovr = inter / areas[j]
-            if dist <= thresh:
-                suppressed[j] = 1
-    return keep[:post_max_size]
