a2a96051dd8d25def0cdd2eedf6018abde52f6f0
wgj839977837 <839977837@qq.com>
2021-11-16 16:24:58 +0800

    Clean tools/cfgs cfg file

--- pcdet/models/dense_heads/__init__.py
+++ pcdet/models/dense_heads/__init__.py
@@ -5,9 +5,6 @@ from .anchor_head_template import AnchorHeadTemplate
 from .point_head_box import PointHeadBox
 from .point_head_simple import PointHeadSimple
 from .point_intra_part_head import PointIntraPartOffsetHead
-from .center_point_head import CenterHead
-from .yolox_head_single_split import YOLOXHeadSingleSplit
-from .yolox_head_single_split_v2 import YOLOXHeadSingleSplitV2
 
 __all__ = {
     'AnchorHeadTemplate': AnchorHeadTemplate,
@@ -17,7 +14,4 @@ __all__ = {
     'PointHeadSimple': PointHeadSimple,
     'PointHeadBox': PointHeadBox,
     'AnchorHeadMulti': AnchorHeadMulti,
-    'CenterHead': CenterHead,
-    'YOLOXHeadSingleSplit':YOLOXHeadSingleSplit,
-    'YOLOXHeadSingleSplitV2':YOLOXHeadSingleSplitV2
 }
